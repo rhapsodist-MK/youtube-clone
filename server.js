76fc37d0e295e549d14e84d261919f89031ac5d2
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-04-03 01:22:22 +0900

    add body-parser, cookie-parser

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const PORT = 3000
 const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/mongo-test'
 
+const { User } = require('./models/user')
+
+
 mongoose.connect(dbConnection, {useNewUrlParser: true})
   .then(() => console.log('DB connected'))
   .catch((err) => console.error(err))
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, userData) => {
+    if (err) return res.json({success: false, err})
+    return res.status(200).json({success: true})
+  })
 })
+
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
